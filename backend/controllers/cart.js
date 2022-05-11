ff2302a250f281571312b3a46107efae815e80bd
HalaNHasan <hala_nazeeh@yahoo.com>
2022-05-11 22:29:04 +0300

    addCartItem & getCartItem functions added

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -31,18 +31,26 @@ const createNewCart= (req, res) => {
 // this function gets all cart items for a specific user:
 const getCartItems= (req, res) => {
     const userId=req.token.userId; //to search in cartModel by a spcific user to render his items later in the front end
-    cartModel.findOne({owner:userId}).then((result)=>{ //to ensure that there is only one cart for each user
-        if(result.cartItems.length ===0){
-              res.status(404).json({
-                  success: false,
-                  message: "No Items Found",
-            })
+    cartModel.findOne({owner:userId}).then((result)=>{
+        if(result){ 
+            if(result.cartItems.length ===0 ){
+                res.status(404).json({
+                    success: false,
+                    message: "No Items Found",
+              })
+          }else if(result.cartItems.length>0){
+              res.status(200).json({
+                    success: true,
+                    cartItems: result.cartItems,
+          })
+      }
+            
         }else{
-            res.status(200).json({
-                  success: true,
-                  cartItems: result.cartItems,
-        })
-    }
+            res.status(404).json({
+                success:false,
+                message:"this user doesn't have a cart!"
+            })
+        }
     }).catch((error)=>{
         res.status(500).json({
             success:false,
@@ -52,7 +60,39 @@ const getCartItems= (req, res) => {
 };
 
 // this function adds an item to the cart by item id:
-const addCartItem= (req, res) => {
+const addCartItem= (req, res) => { 
+    const userId=req.token.userId; 
+    let addedItemId=req.params.id;
+    cartModel.findOne({owner:userId}).then((result1)=>{
+        if(!result1.cartItems.includes(addedItemId)){ // to check that the item is only added one time only to the cart
+           
+    cartModel.updateOne({owner:userId},{$push:{ cartItems:addedItemId}}).then((result)=>{
+        if(result.matchedCount==1){
+            res.status(201).json({
+                success:true,
+                messgae:`item ${addedItemId} added successfully to the cart`
+            })
+        }
+    }).catch((error)=>{
+        res.status(500).json({
+            success:false,
+            error:error.message
+        })
+    })
+
+}else{
+    res.status(406).json({
+        success:false,
+        message:"This item has already been added to your cart!"
+    })
+
+}
+}).catch((error1)=>{
+    res.status(500).json({
+        success:false,
+        error:error1.message
+    })
+})
 };
 
 // this function deletes an item from the cartby its id:
