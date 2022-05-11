f051694a200fea2e15c5d3ba3685ed719afbbf09
HalaNHasan <hala_nazeeh@yahoo.com>
2022-05-11 21:59:19 +0300

    updated itemSchema

--- backend/models/itemSchema.js
+++ backend/models/itemSchema.js
@@ -5,11 +5,13 @@ const itemSchema= new mongoose.Schema({
     owner:{type:mongoose.Schema.Types.ObjectId,ref:"user"},
     item:{type:String,required:true},
     description:{type:String,required:true},
-    exchange:{type:Boolean,required:true},
+    swap:{type:Boolean,required:true},
     sell:{type:Boolean,required:true},
     category:{type:mongoose.Schema.Types.ObjectId,ref:"category"},
-    photos: [{type:String}] //! to be updated later by cloudinary
-    //!rating:to be added later,
+    photos: [{type:String}], //! to be updated later by cloudinary
+    rating:{type:Number,min:0,max:5},
+    swapConfirmed:{type:Boolean,required:true,default:true},
+    isSold:{type:Boolean,default:false},
 })
 
 module.exports=mongoose.model("item",itemSchema);
