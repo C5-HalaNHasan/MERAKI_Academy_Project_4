4d286ff68da554f65c22f0da97dda565637816f9
HalaNHasan <hala_nazeeh@yahoo.com>
2022-05-11 23:26:13 +0300

    the following item functions added: getAllItems,getItemById,updateItemById,deleteItemById,getItemsByRating,

--- backend/controllers/items.js
+++ backend/controllers/items.js
@@ -26,24 +26,108 @@ const createNewItem= (req, res) => {
 };
 
 
-// this function gets all items:
+// this function gets all items: //! to be rendered on the screen
 const getAllItems= (req, res) => {
+   itemModel.find().then((result)=>{
+        if(result.length >0){ 
+          res.status(200).json({
+            success:true,
+            items:result,
+          })
+        }else{
+          res.status(404).json({
+            success:false,
+            message:"no items found"
+          })
+
+        }
+   }).catch((error)=>{
+     res.status(500).json({
+       success:false,
+       error:error.message,
+     })
+   })      
 };
 
 // this function gets an item by its id:
 const getItemById= (req, res) => {
+  let itemId=req.params.id;
+  itemModel.findOne({_id:itemId}).then((result)=>{
+    if(result){
+      res.status(200).json({
+        success:true,
+        item:result
+      })
+    }
+  }).catch((error)=>{
+    res.status(500).json({
+      success:false,
+      error:error.message
+    })
+  })
 };
 
 // this function updates an item by its id:
 const updateItemById= (req, res) => {
+  let{  swapConfirmed,isSold,description,swap,sell,photos} =req.body;
+  let itemId=req.params.id;
+  itemModel.findOneAndUpdate({_id:itemId},{swapConfirmed,isSold,description,swap,sell,photos},{new:true}).then((result)=>{
+    console.log(result)//!
+   if(result){
+      res.status(200).json({
+        success:true,
+        updatedItem:result,
+      })
+    }
+  }).catch((error)=>{
+    res.status(500).json({
+      success:false,
+      error:error.message
+    })
+  })
 };
 
 // this function deletes an item by its id:
 const deleteItemById= (req, res) => {
+  let itemId=req.params.id;
+  itemModel.findOneAndDelete({_id:itemId}).then((result)=>{
+    console.log(result)//!
+   if(result){
+      res.status(200).json({
+        success:true,
+        message:`item ${itemId} has been deleted`
+      })
+    }else{
+      res.status(404).json({
+        success:false,
+        message:`item ${itemId} is not in the data base`
+      })
+
+    }
+  }).catch((error)=>{
+    res.status(500).json({
+      success:false,
+      error:error.message
+    })
+  })
 };
 
 // this function gets all items with a specific rating:
-const getItemsByRating= (req, res) => {
+const getItemsByRating= (req, res) => { //! not tested by postman yet since the rating hasn't beedn added to the items
+  let rating=req.params.rating;
+  itemModel.find({rating:rating}).then((result)=>{
+    if(result){
+      res.status(200).json({
+        success:true,
+        item:result
+      })
+    }
+  }).catch((error)=>{
+    res.status(500).json({
+      success:false,
+      error:error.message
+    })
+  })
 };
 
 
